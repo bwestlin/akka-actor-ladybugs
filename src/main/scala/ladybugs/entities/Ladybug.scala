dade6ad97672ca7d3d183e0ea74a88ea7c7c4a0b
Björn Westlin <bwestlin@gmail.com>
2014-09-10 20:00:13 +0200

    Towards immutable state

--- src/main/scala/ladybugs/entities/Ladybug.scala
+++ src/main/scala/ladybugs/entities/Ladybug.scala
@@ -19,26 +19,29 @@ object Ladybug {
 
 }
 
-class Ladybug(var state: LadybugState) extends Actor with ActorLogging {
+class Ladybug(val initialState: LadybugState) extends Actor with ActorLogging {
 
   import Ladybug._
   import LadybugArena._
 
-  def receive = {
+  override def receive = alive(initialState)
+
+  def alive(state: LadybugState): Receive = {
     case TimeToMove() => {
       val maxAngle = 8
       val angleRadian = (Random.nextDouble() - 0.5) * 2 * maxAngle * Math.PI / 180
-      state = state.copy(direction = state.direction.rotate(angleRadian).normalised)
+      val nextState = state.copy(direction = state.direction.rotate(angleRadian).normalised)
       val speed = 2
 
       sender() ! MovementRequest(
         state.x + state.direction.x * speed,
         state.y + state.direction.y * speed
       )
+      context.become(alive(nextState))
     }
     case MovementRequestResponse(ok, request) => {
       if (ok) {
-        state = state.copy(x = request.x, y = request.y)
+        context.become(alive(state.copy(x = request.x, y = request.y)))
       }
 
       context.system.eventStream.publish(Movement(self, state.x, state.y, state.direction.angle * -180 / Math.PI))
