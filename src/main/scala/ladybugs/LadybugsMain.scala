cf4e6c909f2662451ace713153cb69c4d4c34eed
Björn Westlin <bwestlin@gmail.com>
2015-03-19 00:15:14 +0100

    Switch to using akka.Main and toplevel supervisor actor

--- src/main/scala/ladybugs/LadybugsMain.scala
+++ src/main/scala/ladybugs/LadybugsMain.scala
@@ -1,36 +1,6 @@
 package ladybugs
 
-import akka.actor.ActorSystem
-import akka.io.IO
-import ladybugs.entities.LadybugArena.Spawn
-import ladybugs.entities.LadybugArena
-import ladybugs.http.{LadybugWebsocketUpdater, HttpServer}
-import spray.can.Http
-import spray.can.server.UHttp
-
-import scala.util.Random
-
 object LadybugsMain extends App {
 
-  def doMain() {
-    implicit val system = ActorSystem()
-
-    val server = system.actorOf(HttpServer.props(), "http")
-
-    val port = Option(System.getProperty("http.port")).map(_.toInt).getOrElse(8080)
-
-    IO(UHttp) ! Http.Bind(server, "0.0.0.0", port)
-
-    val arenaWidth = 800
-    val arenaHeight = 600
-
-    val arena = system.actorOf(LadybugArena.props(arenaWidth, arenaHeight), "arena")
-
-    for (_ <- 1 to 20) arena ! Spawn(maybeAge = Some((50 + Random.nextInt(50)) * 10))
-
-    val updater = system.actorOf(LadybugWebsocketUpdater.props(), "updater")
-  }
-
-  // because otherwise we get an ambiguous implicit if doMain is inlined
-  doMain()
+  akka.Main.main(Array("ladybugs.ToplevelSupervisor"))
 }
---
+++ src/main/scala/ladybugs/ToplevelSupervisor.scala
@@ -0,0 +1,36 @@
+package ladybugs
+
+import akka.actor.Actor
+import akka.io.IO
+import ladybugs.entities.LadybugArena
+import ladybugs.entities.LadybugArena.Spawn
+import ladybugs.http.{HttpServer, LadybugWebsocketUpdater}
+import spray.can.Http
+import spray.can.server.UHttp
+
+import scala.util.Random
+
+
+class ToplevelSupervisor extends Actor {
+
+  implicit val system = context.system
+
+  val server = context.actorOf(HttpServer.props(), "http")
+
+  val port = Option(System.getProperty("http.port")).map(_.toInt).getOrElse(8080)
+
+  IO(UHttp) ! Http.Bind(server, "0.0.0.0", port)
+
+  val arenaWidth = 800
+  val arenaHeight = 600
+
+  val arena = context.actorOf(LadybugArena.props(arenaWidth, arenaHeight), "arena")
+
+  for (_ <- 1 to 20) arena ! Spawn(maybeAge = Some((50 + Random.nextInt(50)) * 10))
+
+  val updater = context.actorOf(LadybugWebsocketUpdater.props(), "updater")
+
+  def receive = {
+    case _ =>
+  }
+}
